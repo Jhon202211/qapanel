69784d8a5cb05233ef44be3b859ce4bdd20a5221
YanineMacanaG <yanine@queo.com.co>
2025-11-26 15:09:22 -0500

    fix: desbloquear botones tras tests y codegen con nombre personalizado

--- tests/login_queo.spec.ts
+++ tests/login_queo.spec.ts
@@ -2,15 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://alex.queo.dev/login');
+  await page.goto('https://yanine.queo.dev/login');
   await page.getByRole('textbox', { name: 'Correo electrónico' }).click();
+  await page.getByRole('textbox', { name: 'Correo electrónico' }).click();
+  await page.getByRole('textbox', { name: 'Correo electrónico' }).fill('userrfid2109@refactor.com');
   await page.getByRole('textbox', { name: 'Contraseña' }).click();
+  await page.getByRole('textbox', { name: 'Contraseña' }).fill('userrfid2109');
   await page.getByRole('button', { name: 'Iniciar sesión' }).click();
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).click();
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).fill('alexander@queo.com.co');
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Contraseña' }).fill('123456a');
-  await page.getByRole('button', { name: 'Iniciar sesión' }).click();
-  await page.goto('https://alex.queo.dev/work-areas?searchType=search&');
-  await page.getByRole('link', { name: 'Jhon Alexander Betancur' }).click();
-  await page.getByRole('button', { name: ' Salir' }).click();
+  await page.goto('https://yanine.queo.dev/work-areas?searchType=search&');
 });
